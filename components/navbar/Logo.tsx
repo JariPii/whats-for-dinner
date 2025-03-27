7721f65f3a14511156bea0257bc8b4dd7791d73d
Jari <jaripiilola@gmail.com>
2025-03-27 01:04:21 +0100

    Adds logo adjustments

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Button } from '../ui/button';
-import { VscArchive } from 'react-icons/vsc';
 import logo from '@/public/images/logo.webp';
 
 export const Logo = () => {
   return (
     <Link href='/'>
-      <Image src={logo} alt='Dinner plate' width={100} height={100} />
+      <Image
+        src={logo}
+        alt='Dinner plate'
+        width={150}
+        height={150}
+        className='rounded-3xl'
+      />
     </Link>
   );
 };
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import NavSearch from './NavSearch';
 const Navbar = () => {
   return (
     <nav className='border-b-2'>
-      <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
+      <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-2 gap-4'>
         <Logo />
         <NavSearch />
         <div className='flex gap-4 items-center'>
